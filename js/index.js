c1e241884f9ad30341eb8cf7bd44384a1091d077
lisa213 <joyceoflife@hotmail.com>
2017-07-29 23:48:04 +0800

    no spy, no complete text,no br

---
+++ js/index.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+ // $("#footer").html('<p class = "text-center"> Sponsored by <a href="#">沪交ICP备2010878</a></p><p class = "text-center">&copy;&nbsp;2011 NCT</p>'); 
+/*$('#navbar').click(function (e) {
+  e.preventDefault()
+  $('#navbar li').children('show')
+})
+addClass('active')*/
+});  
